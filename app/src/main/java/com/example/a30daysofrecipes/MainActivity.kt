fbc4cf76f17f4a57744f09349d025077eb04f06b
imadelbouazzaoui <imad.elbouazzaoui.ee@gmail.com>
2023-12-15 23:06:29 +0100

    Choosing colors for the app

--- app/src/main/java/com/example/a30daysofrecipes/MainActivity.kt
+++ app/src/main/java/com/example/a30daysofrecipes/MainActivity.kt
@@ -3,14 +3,23 @@ package com.example.a30daysofrecipes
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.a30daysofrecipes.data.Recipe
+import com.example.a30daysofrecipes.data.recipes
 import com.example.a30daysofrecipes.ui.theme._30DaysOfRecipesTheme
 
 class MainActivity : ComponentActivity() {
@@ -25,10 +34,23 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun ScreenHome(){
-    Card() {
-
+fun RecipeCard(recipe: Recipe){
+    Card {
+        Row{
+            Image(
+                painter = painterResource(id = recipe.imageRes),
+                contentDescription = stringResource(id = recipe.titleRes),
+                modifier = Modifier.size(width = 70.dp, height = 70.dp)
+            )
+            Text(text = stringResource(id = recipe.titleRes))
+        }
     }
-
 }
 
+@Composable
+@Preview(showBackground = false)
+fun RecipeCardPreview() {
+    _30DaysOfRecipesTheme() {
+        RecipeCard(recipe = recipes[0])
+    }
+}
